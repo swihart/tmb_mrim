d889bb419584b8e3d1ff7251adcb48b4b3cf9968
swihart <bruce.swihart@gmail.com>
2017-07-29 00:59:55 -0400

    add bridge CDF

---
+++ pbridge.hpp
@@ -0,0 +1,14 @@
+// Copyright (C) 2017 Bruce Swihart
+// License: GPL-2
+
+/** \file
+    \brief Univariate bridge CDF (Wang & Louis (2003))
+    \ingroup R_style_distribution
+*/
+template<class Type>
+Type pbridge(Type x, Type phi)
+{
+  return 1 - 1/(M_PI * phi) * (M_PI/2 - atan((exp(phi * x) + cos(phi * 
+                M_PI))/sin(phi * M_PI)))
+  }
+VECTORIZE2_tt(pbridge)
