9b01b35bf870de900ff0b0be74fe7c542402d955
swihart <bruce.swihart@gmail.com>
2017-07-29 01:06:16 -0400

    add bridge CDF ;

--- pbridge.hpp
+++ pbridge.hpp
@@ -9,6 +9,6 @@ template<class Type>
 Type pbridge(Type x, Type phi)
 {
   return 1 - 1/(M_PI * phi) * (M_PI/2 - atan((exp(phi * x) + cos(phi * 
-                M_PI))/sin(phi * M_PI)))
+                M_PI))/sin(phi * M_PI)));
   }
 VECTORIZE2_tt(pbridge)
