0fa120df9b5924210bdde21754ed2580330af386
swihart <bruce.swihart@gmail.com>
2023-11-15 09:46:52 -0500

    add dlaplace aka double exponential density

---
+++ dlaplace.hpp
@@ -0,0 +1,17 @@
+
+  /** 	\brief Probability density function of the Laplace distribution.
+   \ingroup R_style_distribution
+  \param mu location
+  \param scale scale
+  \param give_log true if one wants the log-probability, false otherwise.
+  */	
+  template <class Type>
+  Type dlaplace(Type x, Type mu=0, Type scale=1, int give_log=0)
+  {
+    Type logres = -(log(2) + log(scale) + abs(x-mu)/scale);
+    if(!give_log) return exp(logres);
+    else return logres;
+  }
+  
+  // Vectorize dlaplace
+  VECTORIZE4_ttti(dlaplace)
