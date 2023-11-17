57bb1b2bac63aad5da9779cfb0d42becb32b802f
swihart <bruce.swihart@gmail.com>
2023-11-17 16:11:22 -0500

    add exponential power distribution as per CRAN normalp::dnormp()

---
+++ dexppow.hpp
@@ -0,0 +1,23 @@
+
+  /** 	\brief Probability density function of the Laplace distribution.
+   \ingroup R_style_distribution
+  \param mu location
+  \param sigmap scale
+  \param give_log true if one wants the log-probability, false otherwise.
+  */	
+  template <class Type>
+  Type dexppow(Type x, Type mu=0, Type sigmap=1, Type p=2, int give_log=0)
+  {
+
+    Type cost    = 2 * pow(p,1/p) * gamma(1 + 1/p) * sigmap;
+    Type expon1  = pow(sqrt(pow((x - mu),2)),p);
+    Type expon2  = p * pow(sigmap,p);
+    
+    Type res = (1/cost) * exp(-expon1/expon2);
+
+    if(!give_log) return res;
+    else return log(res);
+  }
+  
+  // Vectorize dexppow
+  VECTORIZE5_tttti(dexppow)
